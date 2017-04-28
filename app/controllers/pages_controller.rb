dc30e4c64b9e1a00791a2805be4208e01ef59337
Marcelo Wizenberg <marcelo_wz@hotmail.com>
2017-04-28 19:15:25 +0000

    Deploy to Heroku

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,33 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def home
+    following = Array.new
+    for @f in current_user.following do
+      following.pusg(@f.id)
+    end
+    
+    @posts = Post.where("user_id IN (?)", following)
+    @newPost = Post.new
+  end
+
+  def profile
+    if (User.find_by_username(params[:id]))
+      @username = params[:id]
+    else
+      redirect_to root_path, :notice=> "User not found!"
+    end
+    
+    @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
+    @newPost = Post.new
+    
+    @toFollow = User.all.last(5)
+  end
+  
+  def explore
+    @posts = Post.all
+    @newPost = Post.new
+    @toFollow = User.all.last(5)
+  end
+end
